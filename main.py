6fab21cd22637e6c561de8b21eb65b6e77712048
alliegorical <allison.gretsinger@gmail.com>
2024-08-12 14:26:50 -0400

    added ignore, equipment cost to values

---
+++ main.py
@@ -0,0 +1,6 @@
+import values
+def main():
+    equip_cost = values.init_equip_cost()
+    print(equip_cost)
+
+main()
---
+++ values.py
@@ -0,0 +1,32 @@
+def init_equip_cost():
+    equip_cost = {
+        "inf0": 0.43,
+        "inf1": 0.5,
+        "inf2": 0.58,
+        "inf3": 0.69,
+        "truck": 2.5,
+        "ac1": 4.0,
+        "ac2": 6.0,
+        "ac3": 8.0,
+        "acat": 9.0,
+        "mrart": 12.0,
+        "mech1": 8.0,
+        "mech2": 10.0,
+        "mech3": 12.0,
+        "amech1": 8.0,
+        "amech2": 10.0,
+        "art1": 3.5,
+        "art2": 4.0,
+        "art3": 4.5,
+        "rart1": 4,
+        "rart2": 5,
+        "at1": 4,
+        "at2": 5,
+        "at3": 6,
+        "aa1": 4,
+        "aa2": 5,
+        "aa3": 6,
+        "supp": 4}
+    return equip_cost
+
+def 
